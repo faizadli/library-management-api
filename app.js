da00c9e7e6f1799c1448ad3e127acec8ed3eb7b8
leticiafredella <leticiafredella123@gmail.com>
2025-05-13 18:42:12 +0700

    Setup Swagger documentation with route-based approach

--- app.js
+++ app.js
@@ -1,47 +1,9 @@
 const express = require('express');
-const cors = require('cors');
-const helmet = require('helmet');
-require('dotenv').config();
 const swaggerUi = require('swagger-ui-express');
-const swaggerDocument = require('./swagger.json');
-
-// Import routes
-const adminRoutes = require('./src/routes/adminRoutes');
-const bookRoutes = require('./src/routes/bookRoutes');
-const userRoutes = require('./src/routes/userRoutes');
-const loanRoutes = require('./src/routes/loanRoutes');
+// Update path sesuai struktur folder
+const swaggerSpec = require('./src/routes/swagger');
 
 const app = express();
 
-// Middleware
-app.use(helmet());
-app.use(cors());
-app.use(express.json());
-
-// Routes
-app.use('/api/admin', adminRoutes);
-app.use('/api/books', bookRoutes);
-app.use('/api/users', userRoutes);
-app.use('/api/loans', loanRoutes);
-
-// Tambahkan ini sebelum route lainnya
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
-
-// Root route
-app.get('/', (req, res) => {
-  res.json({ message: 'Welcome to Library Management API' });
-});
-
-// Error handling middleware
-app.use((err, req, res, next) => {
-  console.error(err.stack);
-  res.status(500).json({ message: 'Something went wrong!' });
-});
-
-// Start server
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
-
-module.exports = app;
+// Swagger UI setup
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
